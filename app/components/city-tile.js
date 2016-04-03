462e452adda5d820042f502f04c0cbf9d18ce039
Abigail Rolling <arolling@gmail.com>
2016-04-03 16:04:27 -0700

    Follow lesson on computed properties with bugs

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullLocation: Ember.computed('city.name', 'city.country', function() {
+    return this.get('city.name') + ', ' + this.get('city.country');
+  }),
+  sortDefinition: ['cost:asc'],
+  sortedRentals: Ember.computed.sort('city.rentals', 'sortDefinition'),
+
   actions: {
     delete(city) {
       if (confirm('Are you sure you want to delete this city and all its associated rentals?')) {
